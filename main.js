0833e93e08a226283265fd5cae6ae58342ce894c
ajithasamuel38 <ajithasamuel38@gamil.com>
2024-03-11 12:16:17 +0530

    express

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ app.use((req, res, next)=>{
     next();
 })
 app.use((req, res, next)=>{
-    console.log('In the another middleware');
+    console.log('In another middleware');
     res.send('{ key1: value }');
 })
 
